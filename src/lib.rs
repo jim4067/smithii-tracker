8ff197986c52ec69aa3349dc299c539a8868bc51
Jimii <jimmyimpulse2@gmail.com>
2025-02-24 19:30:27 +0300

    chore: program id

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
+use pinocchio_pubkey::declare_id;
+
 pub mod entrypoint;
 pub mod processor;
+
+declare_id!("GvAgoRguvuNEB6AyMwSdceYGcR1xwR8GQAuobSC79ok9");
--- src/processor.rs
+++ src/processor.rs
@@ -1,18 +1,12 @@
 use pinocchio::{account_info::AccountInfo, program_error::ProgramError, ProgramResult};
-use pinocchio_log::log;
-// use std::convert::TryInto;
 
 #[inline(always)]
 pub fn process_track(_accounts: &[AccountInfo], instruction_data: &[u8]) -> ProgramResult {
-    let timestamp = u64::from_le_bytes(
+    let _timestamp = u64::from_le_bytes(
         instruction_data
             .try_into()
             .map_err(|_error| ProgramError::InvalidAccountData)?,
     );
-
-    log!("timestamp -> {}", timestamp); // ! debug
-
-    // todo: check if PDA is valid
 
     Ok(())
 }
